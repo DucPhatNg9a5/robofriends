68144fcf41491a35c561a8b4a36e16ee5fe28578
phat nguyen <you@example.com>
2020-05-05 23:50:44 -0600

    Updates

---
+++ precache-manifest.59e03aecf8943321001b451ca1e443b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a638cb965ab069988346990c6dc9d1e7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b7dee9fbe471d67fa912",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c850697a9842fe219801",
+    "url": "/robofriends/static/css/main.f498bb57.chunk.css"
+  },
+  {
+    "revision": "b7dee9fbe471d67fa912",
+    "url": "/robofriends/static/js/2.f03e2ba0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f03e2ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c850697a9842fe219801",
+    "url": "/robofriends/static/js/main.ebcdb1c6.chunk.js"
+  },
+  {
+    "revision": "39ee3bfac5940763a3a0",
+    "url": "/robofriends/static/js/runtime-main.57414440.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- src/components/Scroll.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Scroll = (props) => {
-  return (<div style={{overflow: "auto", border: "2px solid black", height: "565px"}}>{props.children}</div>);
-}
-
-export default Scroll;
